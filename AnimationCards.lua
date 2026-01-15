da248bc0683fc456b7013d1fa1ebfd86c2415416
Alvin Dong <alvindong9913@gmail.com>
2026-01-14 19:29:33 -0600

    implement rank anims

--- AnimationCards.lua
+++ AnimationCards.lua
@@ -6,10 +6,6 @@ debugMessage("Launching")
 G.F_NO_ACHIEVEMENTS = false
 local AnimationCards = SMODS.current_mod
 
---- Texturas ---
-SMODS.load_file("textures/planets.lua")()
-SMODS.load_file("textures/cards.lua")()
-
 -- ===========================
 --     LÓGICA DE ANIMACIÓN
 -- ===========================
@@ -33,6 +29,36 @@ if not Aura.update_frame then
 			local fpr = anim.frames_per_row or anim.frames
 			center_obj.pos.x = anim.frame % fpr
 			center_obj.pos.y = math.floor(anim.frame / fpr)
+		end
+	end
+end
+
+--- Texturas ---
+SMODS.load_file("textures/planets.lua")()
+SMODS.load_file("textures/cards.lua")()
+
+--- Hook here
+local _game_update = Game.update
+function Game:update(dt)
+	_game_update(self, dt)
+
+	-- Animar Planetas
+	for key, _ in pairs(AnimatedPlanets) do
+		local center = G.P_CENTERS[key]
+		if center then
+			Aura.update_frame(dt, key, center, AnimatedPlanets)
+		end
+	end
+	for key, _ in pairs(AnimatedCards) do
+		local center = G.P_CARDS[key]
+		if center then
+			if center.lc_atlas ~= "anc_animated_" .. key then
+				center.lc_atlas = "anc_animated_" .. key
+			end
+			if center.hc_atlas ~= "anc_animated_" .. key then
+				center.hc_atlas = "anc_animated_" .. key
+			end
+			Aura.update_frame(dt, key, center, AnimatedCards)
 		end
 	end
 end
